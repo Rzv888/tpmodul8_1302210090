a609c55eeda997d3646fb69ce19e7510aa2b647a
Rzv888 <udinamin445@gmail.com>
2023-04-09 11:51:34 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_1302210090/CovidConfig.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace tpmodul8_1302210090
+{
+    class CovidConfig
+    {
+        private string configFile = "covid config.json";
+
+        public string SatuanSuhu { get; set; } = "celcius";
+        public int BatasHariDeman { get; set; } = 14;
+        public string PesanDitolak { get; set; } = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+        public string PesanDiterima { get; set; } = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+        public CovidConfig()
+        {
+            if (File.Exists(configFile))
+            {
+                string json = File.ReadAllText(configFile);
+                JsonConvert.PopulateObject(json, this);
+            }
+            else
+            {
+                SaveConfig();
+            }
+        }
+
+        public void SaveConfig()
+        {
+            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+            File.WriteAllText(configFile, json);
+        }
+
+        public void UbahSatuan()
+        {
+            if (SatuanSuhu == "celcius")
+            {
+                SatuanSuhu = "fahrenheit";
+            }
+            else
+            {
+                SatuanSuhu = "celcius";
+            }
+
+            SaveConfig();
+        }
+    }
+
+}
--- tpmodul8_1302210090/Program.cs
+++ tpmodul8_1302210090/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul8_1302210090;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CovidConfig config = new CovidConfig();
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.SatuanSuhu}:");
+        double suhu = double.Parse(Console.ReadLine());
+
+        if (config.SatuanSuhu == "fahrenheit")
+        {
+            suhu = (suhu - 32) * 5 / 9;
+        }
+
+        if (suhu >= 36.5 && suhu <= 37.5)
+        {
+            Console.WriteLine($"Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? (max {config.BatasHariDeman} hari)");
+            int hari = int.Parse(Console.ReadLine());
+
+            if (hari < config.BatasHariDeman)
+            {
+                Console.WriteLine(config.PesanDiterima);
+            }
+            else
+            {
+                Console.WriteLine(config.PesanDitolak);
+            }
+        }
+        else
+        {
+            Console.WriteLine(config.PesanDitolak);
+        }
+
+        Console.WriteLine("Apakah anda ingin mengubah satuan suhu? (y/n)");
+        string jawaban = Console.ReadLine();
+
+        if (jawaban.ToLower() == "y")
+        {
+            config.UbahSatuan();
+        }
+    }
+}
